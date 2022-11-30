fab37bace965b6d15c74dd2ffc3133f4349bf613
shanto869 <shantobiswas869@gmail.com>
2022-11-30 18:03:03 +0600

    conditional login and logOut implement

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,15 +1,16 @@
 import { useContext } from 'react';
 import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
 
 const Header = () => {
-
+    const { user, logOut } = useContext(AuthContext)
 
 
     const handleLogOut = () => {
-        // logOut()
-        // .then(() => toast.success('Successfully Sign Out'))
-        // .catch(err => toast.error(err.message))
+        logOut()
+            .then(() => toast.success('Successfully Sign Out'))
+            .catch(err => toast.error(err.message))
     }
 
     const meunItems = <>
@@ -19,9 +20,17 @@ const Header = () => {
 
         <li> <Link to='/add_services' className='btn btn-link text-white nav-font capitalize decoration-none text-lg hover:text-[#b2dd4c]'>Add Services</Link></li>
         <li> <Link to='/my_review' className='btn btn-link text-white nav-font capitalize decoration-none text-lg hover:text-[#b2dd4c]'>My Reviews</Link></li>
-        <li> <Link to='/' onClick={handleLogOut} className='btn btn-link text-white nav-font capitalize decoration-none text-lg hover:text-[#b2dd4c]'>Logout</Link></li>
-        <li> <Link to='/login' className='btn btn-link text-white nav-font capitalize decoration-none text-lg hover:text-[#b2dd4c]'>Login</Link></li>
-        <li> <Link to='/signup' className='btn btn-link text-white nav-font capitalize decoration-none text-lg hover:text-[#b2dd4c]'>Sign Up</Link></li>
+
+        {
+            user?.uid ? <>
+                <li> <Link to='/' onClick={handleLogOut} className='btn btn-link text-white nav-font capitalize decoration-none text-lg hover:text-[#b2dd4c]'>Logout</Link></li>
+
+            </> : <>
+                <li> <Link to='/login' className='btn btn-link text-white nav-font capitalize decoration-none text-lg hover:text-[#b2dd4c]'>Login</Link></li>
+                <li> <Link to='/signup' className='btn btn-link text-white nav-font capitalize decoration-none text-lg hover:text-[#b2dd4c]'>Sign Up</Link></li>
+
+            </>
+        }
 
         {/* </> */}
 
@@ -53,7 +62,7 @@ const Header = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                {/* {
+                {
                     user?.uid && <>
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
@@ -61,7 +70,7 @@ const Header = () => {
                             </div>
                         </label>
                     </>
-                } */}
+                }
             </div>
         </div>
     );
